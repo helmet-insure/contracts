49062945062c0719407517e575cfb8c67e544b8b
helmet-insure <helmet-insure@pm.me>
2020-12-26 06:03:21 +0800

    +HelmetTimelock

--- Helmet.sol
+++ Helmet.sol
@@ -632,16 +632,175 @@ contract ERC20UpgradeSafe is Initializable, ContextUpgradeSafe, IERC20 {
 }
 
 
+/**
+ * @title SafeERC20
+ * @dev Wrappers around ERC20 operations that throw on failure (when the token
+ * contract returns false). Tokens that return no value (and instead revert or
+ * throw on failure) are also supported, non-reverting calls are assumed to be
+ * successful.
+ * To use this library you can add a `using SafeERC20 for ERC20;` statement to your contract,
+ * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
+ */
+library SafeERC20 {
+    using SafeMath for uint256;
+    using Address for address;
+
+    function safeTransfer(IERC20 token, address to, uint256 value) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
+    }
+
+    function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
+    }
+
+    function safeApprove(IERC20 token, address spender, uint256 value) internal {
+        // safeApprove should only be called when setting an initial allowance,
+        // or when resetting it to zero. To increase and decrease it, use
+        // 'safeIncreaseAllowance' and 'safeDecreaseAllowance'
+        // solhint-disable-next-line max-line-length
+        require((value == 0) || (token.allowance(address(this), spender) == 0),
+            "SafeERC20: approve from non-zero to non-zero allowance"
+        );
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
+    }
+
+    function safeIncreaseAllowance(IERC20 token, address spender, uint256 value) internal {
+        uint256 newAllowance = token.allowance(address(this), spender).add(value);
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+    }
+
+    function safeDecreaseAllowance(IERC20 token, address spender, uint256 value) internal {
+        uint256 newAllowance = token.allowance(address(this), spender).sub(value, "SafeERC20: decreased allowance below zero");
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+    }
+
+    /**
+     * @dev Imitates a Solidity high-level call (i.e. a regular function call to a contract), relaxing the requirement
+     * on the return value: the return value is optional (but if data is returned, it must not be false).
+     * @param token The token targeted by the call.
+     * @param data The call data (encoded using abi.encode or one of its variants).
+     */
+    function _callOptionalReturn(IERC20 token, bytes memory data) private {
+        // We need to perform a low level call here, to bypass Solidity's return data size checking mechanism, since
+        // we're implementing it ourselves.
+
+        // A Solidity high level call has three parts:
+        //  1. The target address is checked to verify it contains contract code
+        //  2. The call itself is made, and success asserted
+        //  3. The return value is decoded, which in turn checks the size of the returned data.
+        // solhint-disable-next-line max-line-length
+        require(address(token).isContract(), "SafeERC20: call to non-contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = address(token).call(data);
+        require(success, "SafeERC20: low-level call failed");
+
+        if (returndata.length > 0) { // Return data is optional
+            // solhint-disable-next-line max-line-length
+            require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
+        }
+    }
+}
+
+
+/**
+ * @dev A token holder contract that will allow a beneficiary to extract the
+ * tokens after a given release time.
+ *
+ * Useful for simple vesting schedules like "advisors get all of their tokens
+ * after 1 year".
+ *
+ * For a more complete vesting schedule, see {TokenVesting}.
+ */
+contract TokenTimelockUpgradeSafe is Initializable {
+    using SafeERC20 for IERC20;
+
+    // ERC20 basic token contract being held
+    IERC20 private _token;
+
+    // beneficiary of tokens after they are released
+    address private _beneficiary;
+
+    // timestamp when token release is enabled
+    uint256 private _releaseTime;
+
+
+    function __TokenTimelock_init(IERC20 token, address beneficiary, uint256 releaseTime) internal initializer {
+        __TokenTimelock_init_unchained(token, beneficiary, releaseTime);
+    }
+
+    function __TokenTimelock_init_unchained(IERC20 token, address beneficiary, uint256 releaseTime) internal initializer {
+
+
+        // solhint-disable-next-line not-rely-on-time
+        require(releaseTime > block.timestamp, "TokenTimelock: release time is before current time");
+        _token = token;
+        _beneficiary = beneficiary;
+        _releaseTime = releaseTime;
+
+    }
+
+
+    /**
+     * @return the token being held.
+     */
+    function token() public view returns (IERC20) {
+        return _token;
+    }
+
+    /**
+     * @return the beneficiary of the tokens.
+     */
+    function beneficiary() public view returns (address) {
+        return _beneficiary;
+    }
+
+    /**
+     * @return the time when the tokens are released.
+     */
+    function releaseTime() public view returns (uint256) {
+        return _releaseTime;
+    }
+
+    /**
+     * @notice Transfers tokens held by timelock to beneficiary.
+     */
+    function release() public virtual {
+        // solhint-disable-next-line not-rely-on-time
+        require(block.timestamp >= _releaseTime, "TokenTimelock: current time is before release time");
+
+        uint256 amount = _token.balanceOf(address(this));
+        require(amount > 0, "TokenTimelock: no tokens to release");
+
+        _token.safeTransfer(_beneficiary, amount);
+    }
+
+    uint256[47] private __gap;
+}
+
+
 contract Helmet is ERC20UpgradeSafe, Configurable {
 
-	function initialize(address governor_, address Farm) public initializer {
+	function initialize(address governor_, address locked, address farm, address eco, address lp) public initializer {
 		Governable.initialize(governor_);
-		ERC20UpgradeSafe.__ERC20_init("Helmet Governance Token", "Helmet");
+		ERC20UpgradeSafe.__ERC20_init("Helmet.insure Governance Token", "Helmet");
 		
 		uint8 decimals = 18;
 		_setupDecimals(decimals);
 		
-		_mint(Farm, 100000000 * 10 ** uint256(decimals));       // 100%
+		_mint(locked,   5000_0000 * 10 ** uint256(decimals));       // 50.0%
+		_mint(farm,     3750_0000 * 10 ** uint256(decimals));       // 37.5%
+		_mint(eco,       750_0000 * 10 ** uint256(decimals));       //  7.5%
+		_mint(lp,        500_0000 * 10 ** uint256(decimals));       //  5.0%
 	}
 
+}
+
+
+contract HelmetTimelock is TokenTimelockUpgradeSafe {
+
+    function __HelmetTimelock_init(IERC20 token, address beneficiary, uint256 releaseTime) public initializer {
+        __TokenTimelock_init_unchained(token, beneficiary, releaseTime);
+    }
+    
 }
